a998c8dd139f3af0a080025a32b64d0b4e8c2788
alenazachova <ala.zachova@gmail.com>
2023-10-18 11:31:07 +0200

    update files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./Header";
-import MainContent from "./MainContent";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
